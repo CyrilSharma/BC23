f0b70ced9436c57da6299518f00bf911a371c4ba
CyrilSharma <sharma.cyril@gmail.com>
2023-01-17 16:50:29 -0500

    added jan17

---
+++ src/jan17/Carrier.java
@@ -0,0 +1,329 @@
+package jan17;
+import battlecode.common.*;
+
+// TODO some weird errors with Home being confused with nearest HQ.
+
+public class Carrier extends Robot {
+    int adamantium, mana, elixir;
+    MapLocation wellTarget;
+    MapLocation islandTarget;
+    MapLocation depositLoc = null;
+    // will eventually be replaced with comms.
+    int fleeTurns = 0;
+    MapLocation home;
+    boolean hasAnchor = false;
+    private boolean shouldDeliver = false;
+    enum State {
+        SEARCHING,
+        SEEKING,
+        HARVESTING,
+        DELIVERING,
+        DELIVER_ANCHOR,
+        FLEE
+    }
+    public Carrier(RobotController rc) throws GameActionException {
+        super(rc);
+        communications.findOurHQs();
+        findHome();
+    }
+
+    void run() throws GameActionException {
+        initialize();
+        State state = determineState();
+        rc.setIndicatorString(state.toString());
+        
+        communications.initial();
+        //rc.setIndicatorString("need: " + communications.readResourceNeed());
+        attack();
+        switch (state) {
+            case SEARCHING: search(); break;
+            case SEEKING: seek(); break;
+            case HARVESTING: harvest(); break;
+            case DELIVERING: deliver(); break;
+            case DELIVER_ANCHOR: deliver_anchor(); break;
+            case FLEE: flee(); break;
+            default:
+        }
+        communications.last();
+    }
+
+    void initialize() {
+        adamantium = rc.getResourceAmount(ResourceType.ADAMANTIUM);
+        mana = rc.getResourceAmount(ResourceType.MANA);
+        elixir = rc.getResourceAmount(ResourceType.ELIXIR);
+    }
+
+    // No Seeking state until we have comms.
+    State determineState() throws GameActionException {
+        grab_anchor();
+        for (RobotInfo r: rc.senseNearbyRobots(-1, rc.getTeam().opponent())) {
+            if (Util.isAttacker(r.type)) {
+                fleeTurns = 3;
+                return State.FLEE;
+            }
+        }
+
+        // fleeing but current seeing no enemies, deposit ur stuff.
+        if (shouldDeliver) return State.DELIVERING;
+        if (fleeTurns > 0) {
+            fleeTurns--;
+            if (adamantium + mana + elixir > 10) {
+                shouldDeliver = true;
+            }
+            else return State.FLEE;
+        }
+
+        //System.out.println("good resource: " + communications.readResourceNeed());
+        if (hasAnchor) return State.DELIVER_ANCHOR;
+        if (wellTarget == null && 
+            adamantium == 0 &&
+            mana == 0 && 
+            elixir == 0) {
+            return State.SEARCHING;
+        }
+        
+        if (wellTarget != null && 
+            rc.getLocation().distanceSquaredTo(wellTarget) > 2) {
+            return State.SEEKING;
+        }
+
+        if ((adamantium + mana + elixir) < 39 && 
+            wellTarget != null) {
+            return State.HARVESTING;
+        }
+        if (adamantium + mana + elixir > 0) {
+            return State.DELIVERING;
+        }
+        return State.SEARCHING;
+    }
+
+    void flee() throws GameActionException {
+        findTarget();
+        greedyPath.flee();
+    }
+
+    void search() throws GameActionException{
+        findTarget();
+        if(wellTarget != null){
+            seek();
+            return;
+        }
+        exploration.move(communications.HQs, communications.numHQ);
+        exploration.move(communications.HQs, communications.numHQ);
+    }
+
+    void seek() throws GameActionException {
+        greedyPath.move(wellTarget);
+        greedyPath.move(wellTarget);
+        checkBetterTarget();
+    }
+    //need to redo this part
+
+    void findTarget() throws GameActionException{
+        //basically the idea is, we flip a coin
+        //and based on that, check for certain resources
+        WellInfo[] wells = rc.senseNearbyWells();
+        if (wells.length > 0) {
+            WellTarget best = null;
+            for(WellInfo w : wells){
+                WellTarget cur = new WellTarget(w);
+                if (cur.isBetterThan(best)) best = cur;
+            }
+            // if its crowded and not a good resource use comms.
+            if (!best.crowded() && best.r == communications.readResourceNeed())
+                wellTarget = best.loc;
+            else
+                wellTarget = communications.findBestWell();
+        }
+        else wellTarget = communications.findBestWell(); //null if nothing found
+    }
+
+    void checkBetterTarget() throws GameActionException{
+        WellInfo[] wells = rc.senseNearbyWells();
+        //int ada = communications.getAdamantiumReq(), mana = communications.getManaReq();
+        if (wells.length > 0) {
+            MapLocation bst = null;
+            int dist = rc.getLocation().distanceSquaredTo(wellTarget);
+            for(WellInfo w : wells){
+                if(w.getResourceType() != communications.readResourceNeed()) continue;
+                int d = w.getMapLocation().distanceSquaredTo(communications.findClosestHQto(w.getMapLocation()));
+                if(rc.getLocation().distanceSquaredTo(w.getMapLocation()) + d < dist){
+                   WellTarget uwu = new WellTarget(w);
+                   if(!uwu.crowded()){
+                       bst = uwu.loc;
+                       dist = rc.getLocation().distanceSquaredTo(uwu.loc) + d;
+                   }
+                }
+            }
+            if (bst != null) wellTarget = bst;
+        }
+    }
+
+    class WellTarget {
+        MapLocation loc;
+        int harvestersNear;
+        int distance;
+        ResourceType r;
+        WellTarget(WellInfo w) throws GameActionException{
+            loc = w.getMapLocation();
+            distance = loc.distanceSquaredTo(rc.getLocation()) + loc.distanceSquaredTo(communications.findClosestHQto(loc));
+            harvestersNear = 0;
+            RobotInfo[] robots = rc.senseNearbyRobots();
+            for (RobotInfo r: robots) {
+                if (r.type != RobotType.CARRIER) continue;
+                if (r.location.distanceSquaredTo(loc) <= 4) {
+                    harvestersNear++;
+                }
+            }
+            r = w.getResourceType();
+        }
+
+        boolean crowded() {
+            return harvestersNear>7;
+        }
+
+        boolean bestResource() throws GameActionException {
+            return r == communications.readResourceNeed();
+        }
+
+        boolean isBetterThan(WellTarget wt) throws GameActionException {
+            if (wt == null) return true;
+            if (wt.bestResource() && !bestResource()) return false;
+            if (!wt.bestResource() && bestResource()) return true;
+            if (!wt.crowded() && crowded()) return false;
+            if (wt.crowded() && !crowded()) return true;
+            return distance <= wt.distance; 
+        }
+    }
+
+    // TODO: add some evasive maneuvers
+    void harvest() throws GameActionException {
+        WellInfo wi = rc.senseWell(wellTarget);
+        ResourceType rt = wi.getResourceType();
+        if (rc.canCollectResource(wellTarget, 39-(adamantium + mana + elixir))) {
+            rc.collectResource(wellTarget, 39-(adamantium + mana + elixir));
+            wellTarget = null;
+            communications.resourceNeeded = null;
+        } else if (rc.canCollectResource(wellTarget, -1)) {
+            rc.collectResource(wellTarget, -1);
+        }
+    }
+
+    void deliver() throws GameActionException {
+        int dist = 1000000;
+        for(int i = 0; i < communications.numHQ; i++){
+            if(rc.getLocation().distanceSquaredTo(communications.HQs[i]) < dist){
+                depositLoc = communications.HQs[i];
+                dist = rc.getLocation().distanceSquaredTo(communications.HQs[i]);
+            }
+        }
+        if (depositLoc.distanceSquaredTo(rc.getLocation()) > 1) {
+            greedyPath.move(depositLoc);
+        } else {
+            ResourceType[] resources = {ResourceType.ADAMANTIUM, ResourceType.ELIXIR, ResourceType.MANA};
+            for (ResourceType r: resources) {
+                if (rc.getResourceAmount(r) == 0) continue;
+                if (rc.canTransferResource(depositLoc, r, rc.getResourceAmount(r))) {
+                    rc.transferResource(depositLoc, r, rc.getResourceAmount(r));
+                    shouldDeliver = false;
+                }
+            }
+        }
+    }
+
+    void grab_anchor() throws GameActionException {
+        if (depositLoc == null) return;
+        if (rc.canTakeAnchor(depositLoc, Anchor.STANDARD)) {
+            rc.takeAnchor(depositLoc, Anchor.STANDARD);
+            hasAnchor = true;
+        }
+    }
+
+    void deliver_anchor() throws GameActionException {
+        if (islandTarget == null) {
+            islandTarget = findIslandTarget();
+            if (islandTarget == null) 
+                exploration.move(communications.HQs, communications.numHQ);
+        }
+        if (islandTarget != null) {
+            if (rc.getLocation().distanceSquaredTo(islandTarget) > 0) {
+                greedyPath.move(islandTarget);
+            } else {
+                if (rc.canPlaceAnchor()) {
+                    rc.placeAnchor();
+                    islandTarget = null;
+                    hasAnchor = false;
+                }
+            }
+        }
+    }
+
+    MapLocation findIslandTarget() throws GameActionException {
+        int[] islands = rc.senseNearbyIslands();
+        MapLocation closestTarget = null;
+        int d = 100000;
+        for (int idx: islands) {
+            if (rc.senseAnchor(idx) != null) continue;
+            MapLocation[] spots = rc.senseNearbyIslandLocations(idx);
+            for (MapLocation spot: spots) {
+                if (rc.getLocation().distanceSquaredTo(spot) < d) {
+                    closestTarget = spot;
+                }
+            }
+        }
+        return closestTarget;
+    }
+
+    void findHome() throws GameActionException {
+        RobotInfo[] robots = rc.senseNearbyRobots(rc.getType().visionRadiusSquared, rc.getTeam());
+        for (RobotInfo r: robots) {
+            if (r.type == RobotType.HEADQUARTERS) {
+                home = r.location;
+                break;
+            }
+        }
+    }
+
+    void attack() throws GameActionException {
+        RobotInfo best = util.getBestAttackTarget();
+        if (best == null) return;
+        if (!Util.isAttacker(best.type)) return;
+        if (rc.canAttack(best.location)) {
+            int total = adamantium + mana + elixir;
+            if (total > 5 && total < 20) {
+                rc.attack(best.location);
+                shouldDeliver = false;
+            }
+        }
+    }
+
+    class AttackTarget {
+        MapLocation loc;
+        boolean inRange;
+        int priority;
+        int health;
+
+        AttackTarget(RobotInfo r) {
+            loc = r.location;
+            switch (r.getType()) {
+                case BOOSTER: priority=4; break;
+                case AMPLIFIER: priority=3; break;
+                case HEADQUARTERS: priority=1; break;
+                case CARRIER: priority=2; break;
+                case LAUNCHER: priority=6; break;
+                case DESTABILIZER: priority=5; break;
+            }
+            health = r.health;
+            inRange = (rc.getLocation().distanceSquaredTo(loc) < rc.getType().actionRadiusSquared);
+        }
+
+        boolean isBetterThan(AttackTarget at) {
+            if (at == null) return true;
+            if (at.inRange && !inRange) return false;
+            if (!at.inRange && inRange) return true;
+            if (at.priority > priority) return false;
+            if (at.priority < priority) return true;
+            return health <= at.health;
+        }
+    }
+}
---
+++ src/jan17/Launcher.java
@@ -0,0 +1,265 @@
+package jan17;
+import battlecode.common.*;
+
+// Control flow.
+// If no targets in range continue exploring, or seeking out some other target.
+// If there are enemies, but they're all nice, path towards them aggresively.
+// If there are attacking enemies, run attack micro.
+public class Launcher extends Robot {
+    RobotInfo[] enemies;
+    MapLocation myloc;
+    boolean hurt = false;
+    enum State {
+        ATTACK,
+        HUNT,
+        EXPLORE,
+        DEFEND
+    }
+
+    public Launcher(RobotController rc) throws GameActionException {
+        super(rc);
+        communications.findOurHQs();
+    }
+    void run() throws GameActionException {
+        initialize();
+        communications.initial();
+        State state = determineState();
+        rc.setIndicatorString(state.toString());
+        doAttack(true);
+        switch (state) {
+            case ATTACK: attack(); break;
+            case HUNT: hunt(); break;
+            case EXPLORE: explore(); break;
+            case DEFEND: defend(); break;
+        }
+        doAttack(false);
+        communications.last();
+    }
+
+    void initialize() throws GameActionException {
+        enemies = rc.senseNearbyRobots(rc.getType().visionRadiusSquared, rc.getTeam().opponent());
+        myloc = rc.getLocation();
+        if (rc.getHealth() < 20) 
+            hurt = true;
+    }
+
+    void doAttack(boolean attackers) throws GameActionException {
+        RobotInfo r = util.getBestAttackTarget();
+        if (r == null) return;
+        if (attackers && !Util.isAttacker(r.type)) return;
+        rc.setIndicatorString("Attacking: " + r.location);
+        if (r != null && rc.canAttack(r.location)) 
+            rc.attack(r.location);
+    }
+
+    State determineState() throws GameActionException {
+        for (RobotInfo e : enemies) {
+            if (e.type != RobotType.HEADQUARTERS) return State.ATTACK;
+        }
+        MapLocation m = communications.findBestAttackTarget();
+        if (m != null && !rc.canSenseLocation(m)) return State.HUNT;
+        if (rc.getLocation().distanceSquaredTo(communications.findClosestHQ()) >= 49) return State.DEFEND;
+        return State.EXPLORE;
+    }
+
+    void attack() throws GameActionException {
+        boolean attacker = false;
+        for (RobotInfo e: enemies) {
+            if (Util.isAttacker(e.getType())) {
+                attacker = true;
+            }
+        }
+        if (!attacker) {
+            RobotInfo r = util.getBestAttackTarget();
+            follow(r.location);
+        } else {
+            maneuver();
+        }
+    }
+
+    // Relies on comms.
+    void hunt() throws GameActionException {
+        MapLocation huntTarget = communications.findBestAttackTarget();
+        rc.setIndicatorDot(rc.getLocation(), 0, 0, 0);
+        rc.setIndicatorLine(rc.getLocation(), huntTarget, 0, 0, 0);
+        if (rc.getLocation().distanceSquaredTo(huntTarget) > (rc.getType().visionRadiusSquared + 20))
+            greedyPath.move(huntTarget);
+        else if (rc.getRoundNum()%2 == 0) 
+            greedyPath.move(huntTarget);
+    }
+
+    // Relies on exploration code.
+    void explore() throws GameActionException{
+        exploration.moveLauncher(communications.HQs, communications.numHQ);
+    }
+
+    void defend() throws GameActionException {
+        greedyPath.move(communications.findClosestHQ());
+    }
+
+    void follow(MapLocation m) throws GameActionException {
+        ChaseTarget best = null;
+        for (Direction dir: directions) {
+            ChaseTarget cur = new ChaseTarget(dir, m);
+            if (cur.isBetterThan(best)) best = cur;
+        }
+        if (rc.canMove(best.dir))
+            rc.move(best.dir);
+    }
+
+    void maneuver() throws GameActionException {
+        MicroTarget[] microtargets = new MicroTarget[9];
+        for (Direction d: directions) {
+            microtargets[d.ordinal()] = new MicroTarget(d);
+        }
+
+        for (RobotInfo r: rc.senseNearbyRobots()) {
+            if (Clock.getBytecodesLeft() < 1000) break;
+            for (Direction d: directions) {
+                if (r.team == rc.getTeam()) microtargets[d.ordinal()].addAlly(r);
+                else microtargets[d.ordinal()].addEnemy(r);
+            }
+        }
+
+        MicroTarget best = microtargets[0];
+        for (int i = 0; i < 9; i++) {
+            if (microtargets[i].isBetterThan(best))
+                best = microtargets[i];
+        }
+        if (rc.canMove(best.dir)) rc.move(best.dir);
+    }
+
+    AttackTarget getBestAttackTarget() {
+        AttackTarget best = null;
+        for (RobotInfo e: enemies) {
+            AttackTarget cur = new AttackTarget(e);
+            if (cur.isBetterThan(best)) best = cur;
+        }
+        return best;
+    }
+
+    // Choose best candidate for maneuvering in close encounters.
+    class MicroTarget {
+        Direction dir;
+        double net_dps = 0;
+        double dps_targetting = 0;
+        double dps_defending = 0;
+        int minDistToEnemy = 100000;
+        boolean canMove;
+        MapLocation nloc;
+
+        MicroTarget(Direction dir) throws GameActionException {
+            this.dir = dir;
+            net_dps -= ((double) rc.getType().damage) * (1.0 / rc.senseCooldownMultiplier(myloc));
+            nloc = myloc.add(dir);
+            canMove = rc.canMove(dir);
+            if (canMove) {
+                MapInfo mi = rc.senseMapInfo(nloc);
+                nloc.add(mi.getCurrentDirection());
+            }
+        }
+        
+        void addEnemy(RobotInfo r) throws GameActionException {
+            if (!Util.isAttacker(r.type)) return;
+            MapLocation m = r.location;
+            MapInfo mi = rc.senseMapInfo(m);
+            // ignore boosting effects of other units for now.
+            double cooldown = mi.getCooldownMultiplier(rc.getTeam().opponent());
+            if (r.type == RobotType.BOOSTER) {
+                int d = nloc.distanceSquaredTo(m);
+                if (d <= r.type.actionRadiusSquared) 
+                    net_dps += (double) r.type.damage * (1.0 / cooldown);
+                if (d <= r.type.visionRadiusSquared)
+                    dps_targetting += (double) r.type.damage * (1.0 / cooldown);;
+                if (d <= minDistToEnemy)
+                    minDistToEnemy = d;
+            }
+        }
+        
+        void addAlly(RobotInfo r) throws GameActionException {
+            if (Util.isAttacker(r.type)) {
+                MapInfo mi = rc.senseMapInfo(r.location);
+                double cooldown = mi.getCooldownMultiplier(rc.getTeam());
+                dps_defending += (double) r.type.damage * (1.0 / cooldown);
+            }
+        }
+       
+        int safe() {
+            if (net_dps > 0) return 1;
+            if (dps_defending < dps_targetting) return 2;
+            return 3;
+        }
+
+        boolean isBetterThan(MicroTarget mt) {
+            if (mt.canMove && !canMove) return false;
+            if (!mt.canMove && canMove) return true;
+            
+            // the idea here is attack first, then move out of range.
+            if (mt.safe() > safe()) return false;
+            if (mt.safe() < safe()) return true;
+
+            // If hurt move to where enemies are targetting the least.
+            if (hurt) {
+                if (mt.dps_targetting < dps_targetting) return false;
+                if (mt.dps_targetting > dps_targetting) return true;
+                // run away!!!!
+                return minDistToEnemy >= mt.minDistToEnemy;
+            }
+
+            // get as far away from enemies while still being in range.
+            if (minDistToEnemy <= rc.getType().actionRadiusSquared)
+                return minDistToEnemy >= mt.minDistToEnemy;
+            else
+                return minDistToEnemy <= mt.minDistToEnemy;
+        }
+    }
+    // Choose best candidate to attack.
+    class AttackTarget {
+        MapLocation loc;
+        int priority;
+        int health;
+
+        AttackTarget(RobotInfo r) {
+            loc = r.location;
+            switch (r.getType()) {
+                case BOOSTER: priority=4; break;
+                case AMPLIFIER: priority=3; break;
+                case HEADQUARTERS: priority=1; break;
+                case CARRIER: priority=2; break;
+                case LAUNCHER: priority=6; break;
+                case DESTABILIZER: priority=5; break;
+            }
+            health = r.health;
+        }
+
+        boolean isBetterThan(AttackTarget at) {
+            if (at == null) return true;
+            if (at.priority > priority) return false;
+            if (at.priority < priority) return true;
+            return health <= at.health;
+        }
+    }
+
+    // Choose best square to chase a defenseless target.
+    class ChaseTarget {
+        int d;
+        Direction dir;
+        boolean canMove;
+
+        ChaseTarget(Direction dir, MapLocation m) {
+            this.dir = dir;
+            MapLocation newloc = myloc.add(dir);
+            d = newloc.distanceSquaredTo(m);
+            canMove = rc.canMove(dir);
+        }
+
+        boolean isBetterThan(ChaseTarget ct) {
+            if (ct == null) return true;
+            if (ct.canMove && !canMove) return false;
+            if (!ct.canMove && canMove) return true;
+            // if sufficently close, do not move closer.
+            if (d <= rc.getType().actionRadiusSquared / 2) return true;
+            return d <= ct.d;
+        }
+    }
+}
