10bfe658263855cbb3c952d4504c256e4bb8512c
CyrilSharma <sharma.cyril@gmail.com>
2023-02-03 10:49:18 -0500

    added feb2

---
+++ src/feb2/Constants.java
@@ -0,0 +1,7 @@
+package feb2;
+
+public class Constants {
+    public static int BYTECODE_AMPLIFY = 1000;
+    public static int REPORT_FREQ = 5;
+    public static int ATTACK_REFRESH = 10;
+}
---
+++ src/feb2/Destabilizer.java
@@ -0,0 +1,9 @@
+package feb2;
+import battlecode.common.*;
+
+public class Destabilizer extends Robot {
+    public Destabilizer(RobotController rc) {
+        super(rc);
+    }
+    void run() {;}
+}
