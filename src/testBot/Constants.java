e3da89f42b275edf3e4448eb2945ea6babad55f1
Egor Gagushin <eggag03@gmail.com>
2023-02-03 11:04:11 -0600

    remove testBot

--- src/testBot/Constants.java
+++
@@ -1,7 +0,0 @@
-package testBot;
-
-public class Constants {
-    public static int BYTECODE_AMPLIFY = 1000;
-    public static int REPORT_FREQ = 5;
-    public static int ATTACK_REFRESH = 10;
-}
--- src/testBot/Destabilizer.java
+++
@@ -1,9 +0,0 @@
-package testBot;
-import battlecode.common.*;
-
-public class Destabilizer extends Robot {
-    public Destabilizer(RobotController rc) {
-        super(rc);
-    }
-    void run() {;}
-}
